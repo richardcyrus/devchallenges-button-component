17aa427803a0a5146ae96a6b1482da8939c10337
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-03-10 17:21:58 -0500

    feat: create base page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
 function App() {
-
   return (
-    <div>Hello, world!
+    <div className="container">
+      <section className="sidebar">
+        <h1 className="nameplate">
+          <span className="colored-nameplate">Dev</span>challenges.io
+        </h1>
+        <nav className="nav-container">
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <button type="button" className="button nav-link">
+                Inputs
+              </button>
+            </li>
+            <li className="nav-item nav-active">
+              <button type="button" className="button nav-link">
+                Buttons
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </section>
+      <section className="content">
+        <div className="main">
+          <h2 className="section-title">Buttons</h2>
+        </div>
+        <footer>
+          <div className="attribution">
+            Challenge by{' '}
+            <a
+              href="https://devchallenges.io/challenges/ohgVTyJCbm5OZyTB2gNY"
+              target="_blank"
+            >
+              devChallenges.io
+            </a>
+            . Coded by{' '}
+            <a
+              href="https://www.richardcyrus.com"
+              className="text-bold line-link"
+            >
+              Richard Cyrus
+            </a>
+            .
+          </div>
+        </footer>
+      </section>
     </div>
   )
 }
